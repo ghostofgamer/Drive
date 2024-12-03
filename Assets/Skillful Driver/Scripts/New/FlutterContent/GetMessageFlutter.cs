3cc6ee15f7c5378ff24ca17237136366f78632e2
ghostofgamer <vadimcapital@mail.ru>
2024-12-03 12:03:55 +0400

    AddGetMessageFlutter

---
+++ Assets/Skillful Driver/Scripts/New/FlutterContent/GetMessageFlutter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMessageFlutter : MonoBehaviour
+{
+    public void GetToken(string token)
+    {
+        Debug.Log(token);
+    }
+}
